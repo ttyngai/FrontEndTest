5a34eaed0aa220b28be6326bf168eed6eb815709
ttyngai <ttyngai@gmail.com>
2023-04-27 23:54:22 -0700

    update

--- tests/test.js
+++ tests/test.js
@@ -3,18 +3,55 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const loginAccount = 'timothy.ngai@primedatalytics.com';
 const loginPassword = 'password';
 
-const login = async () => {
+const test = async () => {
   let driver = await new Builder().forBrowser('chrome').build();
+  const waitOut = async (time) => {
+    await driver.sleep(time);
+  };
 
   await driver.get('http://localhost:3000/dashboard/');
 
-  await driver.findElement(By.id('navLogin')).click();
+  const login = async () => {
+    await driver.findElement(By.id('navLogin')).click();
+    await waitOut(100);
+    await driver.findElement(By.id('form1Example1')).sendKeys(loginAccount);
+    await waitOut(100);
+    await driver.findElement(By.id('form1Example2')).sendKeys(loginPassword);
+    await waitOut(100);
+    await driver.findElement(By.id('signIn')).click();
+    await waitOut(100);
+  };
 
-  await driver.findElement(By.id('form1Example1')).sendKeys(loginAccount);
+  const checkSettings = async () => {
+    await driver.wait(until.elementLocated(By.id('settings')));
+    await waitOut(1000);
+    await driver.findElement(By.id('settings')).click();
+    await waitOut(1000);
+    await driver.findElement(By.id('settingSwitchLoadFPD')).click();
+    await waitOut(500);
+    await driver.findElement(By.id('settingSwitchLoadFPD')).click();
+    await waitOut(500);
+    await driver.findElement(By.id('settingSwitchDarkMode')).click();
+    await waitOut(500);
+    await driver.findElement(By.id('settingSwitchDarkMode')).click();
+    await waitOut(500);
+    await driver.findElement(By.id('settingsDone')).click();
+    await waitOut(500);
+  };
 
-  await driver.findElement(By.id('form1Example2')).sendKeys(loginPassword);
+  const checkAppSuiteAudienceConstructor = async () => {
+    await driver.findElement(By.id('appSuiteAudienceConstructor')).click();
+    await waitOut(500);
+  };
 
-  await driver.findElement(By.id('signIn')).click();
+  await login();
+  await checkSettings();
+  await checkAppSuiteAudienceConstructor();
+
+  //Final Exit
+  await waitOut(10000);
+  console.log('exit');
+  driver.quit();
 };
 
-login();
+test();
