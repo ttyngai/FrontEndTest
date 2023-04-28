bfa512bc3932a3b93c892ccfc93ad047394cdc37
ttyngai <ttyngai@gmail.com>
2023-04-28 10:03:47 -0700

    update4

--- tests/test.js
+++ tests/test.js
@@ -5,47 +5,70 @@ const loginPassword = 'password';
 
 const test = async () => {
   let driver = await new Builder().forBrowser('chrome').build();
+  //Reusables
   const wait = async (time) => {
     await driver.sleep(time);
   };
 
+  const defaultWaitTime = 100;
+
+  const clickId = async (id, endWaitTime = defaultWaitTime) => {
+    await driver.findElement(By.id(id)).click();
+    await wait(endWaitTime);
+  };
+  const inputKeyId = async (id, input, endWaitTime = defaultWaitTime) => {
+    await driver.findElement(By.id(id)).sendKeys(input);
+    await wait(endWaitTime);
+  };
+  const clearKeyId = async (id, endWaitTime = defaultWaitTime) => {
+    await driver.findElement(By.id(id)).clear();
+    await wait(endWaitTime);
+  };
+  const backSpaceKeyId = async (
+    id,
+    endWaitTime = defaultWaitTime,
+    keySpeed = 100
+  ) => {
+    let attribute = await driver.findElement(By.id(id)).getAttribute('value');
+    for (let i = 0; i < attribute.length; i++) {
+      await driver.findElement(By.id(id)).sendKeys(Key.BACK_SPACE);
+      await wait(keySpeed);
+    }
+    await wait(endWaitTime);
+  };
+  const waitForId = async (id, endWaitTime = defaultWaitTime) => {
+    await driver.wait(until.elementLocated(By.id(id)));
+    await wait(endWaitTime);
+  };
+
+  //Starter
   await driver.get('http://localhost:3000/dashboard/');
   await wait(500);
+
+  //Sequences
   const login = async () => {
-    await driver.findElement(By.id('navLogin')).click();
-    await wait(100);
-    await driver.findElement(By.id('form1Example1')).sendKeys(loginAccount);
-    await wait(100);
-    await driver.findElement(By.id('form1Example2')).sendKeys(loginPassword);
-    await wait(100);
-    await driver.findElement(By.id('signIn')).click();
-    await wait(100);
+    await clickId('navLogin');
+    await inputKeyId('form1Example1', loginAccount);
+    await inputKeyId('form1Example2', loginPassword);
+    await clickId('signIn');
   };
 
   const checkSettings = async () => {
-    await driver.wait(until.elementLocated(By.id('settings')));
-    await wait(500);
-    await driver.findElement(By.id('settings')).click();
-    await wait(1000);
-    await driver.findElement(By.id('settingSwitchLoadFPD')).click();
-    await wait(500);
-    await driver.findElement(By.id('settingSwitchLoadFPD')).click();
-    await wait(500);
-    await driver.findElement(By.id('settingSwitchDarkMode')).click();
-    await wait(500);
-    await driver.findElement(By.id('settingSwitchDarkMode')).click();
-    await wait(500);
-    await driver.findElement(By.id('settingsDone')).click();
-    await wait(500);
+    await waitForId('settings', 500);
+    await clickId('settings', 1000);
+    await clickId('settingSwitchLoadFPD', 500);
+    await clickId('settingSwitchLoadFPD', 500);
+    await clickId('settingSwitchDarkMode', 500);
+    await clickId('settingSwitchDarkMode', 500);
+    await clickId('settingsDone', 500);
   };
 
   const checkAppSuiteAudienceConstructor = async () => {
-    await driver.wait(until.elementLocated(By.id('settings')));
-    await wait(500);
-    await driver.findElement(By.id('appSuiteAudienceConstructor')).click();
-    await wait(500);
+    await waitForId('appSuiteAudienceConstructor', 500);
+    await clickId('appSuiteAudienceConstructor', 500);
+
     //menu mode
-    await driver.wait(until.elementLocated(By.id('constructorMenuContainer')));
+    await waitForId('constructorMenuContainer');
     //Check menu
     let parentElement = await driver.findElement(
       By.id('constructorMenuContainer')
@@ -54,68 +77,47 @@ const test = async () => {
 
     if (childElements.length > 0) {
       childElements.forEach(async (each, idx) => {
-        await driver.wait(until.elementLocated(By.id(`constructorMenu${idx}`)));
-        await wait(100 * idx);
-        await driver.findElement(By.id(`constructorMenu${idx}`)).click();
+        await waitForId(`constructorMenu${idx}`, 50 * idx);
+        await clickId(`constructorMenu${idx}`);
       });
     }
+    await wait(childElements.length * 50);
 
-    await wait(childElements.length * 100);
-    await driver.wait(
-      until.elementLocated(By.id('audienceConstructorKeywordButton'))
-    );
+    await waitForId('audienceConstructorKeywordButton');
     // await wait(500);
-    await driver
-      .findElement(By.id('audienceConstructorMenuInput'))
-      .sendKeys('car');
-    await wait(500);
-    await driver.findElement(By.id('audienceConstructorMenuInput')).clear();
-    await driver
-      .findElement(By.id('audienceConstructorMenuInput'))
-      .sendKeys('vehicle');
-    await wait(500);
-    await driver.findElement(By.id('audienceConstructorMenuInput')).clear();
-    await driver
-      .findElement(By.id('audienceConstructorMenuInput'))
-      .sendKeys('bank');
-    await wait(500);
-    for (let i = 0; i < 5; i++) {
-      await driver
-        .findElement(By.id('audienceConstructorMenuInput'))
-        .sendKeys(Key.BACK_SPACE);
-    }
-    await wait(500);
+    await inputKeyId('audienceConstructorMenuInput', 'car', 500);
+    await backSpaceKeyId('audienceConstructorMenuInput', 500);
+    await inputKeyId('audienceConstructorMenuInput', 'bank', 500);
+    await backSpaceKeyId('audienceConstructorMenuInput', 500);
+    await inputKeyId('audienceConstructorMenuInput', 'coffee', 500);
+    await backSpaceKeyId('audienceConstructorMenuInput', 500);
+
     //Thought Expander
-    await driver.wait(
-      until.elementLocated(By.id('audienceConstructorThoughtExpanderButton'))
-    );
-    await wait(500);
-    await driver
-      .findElement(By.id(`audienceConstructorThoughtExpanderButton`))
-      .click();
+    await waitForId('audienceConstructorThoughtExpanderButton', 500);
+    await clickId('audienceConstructorThoughtExpanderButton', 500);
+    await inputKeyId('audienceConstructorMenuInput', 'car', 1000);
+    await clickId('audienceConstructorSearchButton', 1500);
 
-    await wait(500);
-    await driver
-      .findElement(By.id('audienceConstructorMenuInput'))
-      .sendKeys('car');
-    await driver.findElement(By.id(`audienceConstructorSearchButton`)).click();
-    await wait(1000);
     //Get Demographics Age
-    await driver.findElement(By.id(`audienceConstructorKeywordButton`)).click();
-    await wait(500);
-    for (let i = 0; i < 5; i++) {
-      await driver
-        .findElement(By.id('audienceConstructorMenuInput'))
-        .sendKeys(Key.BACK_SPACE);
+    await clickId('audienceConstructorKeywordButton', 500);
+    await backSpaceKeyId('audienceConstructorMenuInput', 500);
+    await clickId('eachAudTitle2', 500);
+    await clickId('eachTopic0', 500);
+    await clickId('audienceConstructorGroupBox', 500); //Put into group box
+
+    //Select in checkbox
+    await waitForId('audienceConstructorOptionContainer', 100);
+    // Clicks select All
+    await clickId('audienceConstructorOptionSelectAll', 500);
+    await clickId('audienceConstructorOptionSelectAll', 500);
+
+    //Clicks a few
+    for (let i = 0; i < 6; i++) {
+      await clickId(`audienceConstructorOption${i}`, 100);
     }
     await wait(500);
 
-    await driver.findElement(By.id(`eachAudTitle2`)).click();
-    await wait(500);
-
-    await driver.findElement(By.id(`eachTopic0`)).click();
-    await wait(500);
-    await driver.findElement(By.id(`audienceConstructorGroupBox`)).click();
+    await clickId('audienceConstructorOptionDone', 100);
   };
 
   //Activation
